9570102e22f8371655092cf0fd94237c2118c60c
Mattia Piccinetti <mattiapiccinetti@users.noreply.github.com>
2021-12-27 12:52:28 +0100

    Add lonfo function

--- zsh/zsh-functions.sh
+++ zsh/zsh-functions.sh
@@ -4,3 +4,32 @@ path () {
 	echo "$(pwd)/$1"
 }
 
+lonfo () {
+	cat << EOF
+
+
+	Il Lonfo
+
+	Il Lonfo non vaterca né gluisce
+	e molto raramente barigatta,
+	ma quando soffia il bego a bisce bisce
+	sdilenca un poco e gnagio s'archipatta.
+	È frusco il Lonfo! È pieno di lupigna
+	arrafferia malversa e sofolenta!
+	Se cionfi ti sbiduglia e ti arrupigna
+	se lugri ti botalla e ti criventa.
+
+	Eppure il vecchio Lonfo ammargelluto
+	che bete e zugghia e fonca nei trombazzi
+	fa lègica busìa, fa gisbuto;
+
+	e quasi quasi in segno di sberdazzi
+	gli affarferesti un gniffo. Ma lui zuto
+	t'alloppa, ti sbernecchia; e tu l'accazzi.
+
+
+	Fosco Maraini
+
+
+EOF
+}
